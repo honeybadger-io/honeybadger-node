f951a23984eaafc75939d9c7bfddf27f26020023
Joshua Wood <josh@hintmedia.com>
2016-04-07 17:05:39 -0700

    Add remaining suites.

--- test/honeybadger.js
+++ test/honeybadger.js
@@ -296,4 +296,161 @@ describe('Honeybadger', function () {
       });
     });
   });
+
+
+ suite('Passing contextual metadata with an error', function () {
+    var hb = new Singleton.factory({ apiKey: 'fake api key' });
+
+    var meta = {
+      context: {
+        uid: '0xdeadbeef',
+        email: 'test@testing.biz'
+      },
+      session: {
+        id: '1234567890',
+        data: {a: 1, b: 2, c: 3}
+      },
+      params: {
+        arg1: 'hello'
+      },
+      cookies: {
+        omnom: 'nom'
+      }
+    };
+
+    test('successfully sends the payload', function (done) {
+      payloadCount = payloads.length;
+      hb.once('sent', function () {
+        var p;
+        assert(payloads.length === (payloadCount + 1), 'payload not sent');
+        p = payloads[payloads.length - 1];
+        assert(p.error.message === 'test error 4', 'payload incorrect');
+        done();
+      });
+      hb.notify(new Error('test error 4'), meta);
+    });
+
+    test('The contextual metadata is passed correctly', function () {
+      hb.once('sent', function () {
+        var p = payloads[payloads.length - 1];
+        assert.deepEqual(p.request, meta, 'Metadata incorrect');
+        done();
+      });
+    });
+  });
+
+  suite('Creating a Badger with cgi_data meta', function () {
+    var hb = Singleton.factory({
+      apiKey: 'fake api key'
+    });
+
+    var sampleCGIData = {
+      'server-software': 'Whatever server',
+      'custom': 'custom fields with custom DATA, !#&*'
+    };
+
+    test('successfully sends the payload', function (done) {
+      payloadCount = payloads.length;
+      hb.once('sent', function () {
+        var p;
+        assert(payloads.length === (payloadCount + 1), 'payload not sent');
+        p = payloads[payloads.length - 1];
+        assert(p.error.message === 'test error 5', 'payload incorrect');
+        done();
+      });
+      hb.notify(new Error('test error 5'), { cgi_data: sampleCGIData });
+    });
+
+    test('transforms data keys according to the RFC 3875', function () {
+      hb.once('sent', function () {
+        var n;
+        n = payloads[payloads.length - 1].request.cgi_data;
+        assert(n['SERVER_SOFTWARE'] === sampleCGIData['server-software'], 'server-software not set');
+        assert(n['CUSTOM'] === sampleCGIData['custom'], 'custom not set');
+        assert(('custom' in n) === false, 'untransformed keys present in payload');
+      });
+    });
+  });
+
+  suite('Creating a Badger with headers meta', function () {
+    var hb = Singleton.factory({
+      apiKey: 'fake api key'
+    });
+
+    var sampleHeaders = {
+      'x-forwarded-for': '1.2.3.4',
+      'user-agent': 'Mozilla 5.0',
+      'cookie': 'a=b'
+    };
+
+    test('successfully sends the payload', function (done) {
+      payloadCount = payloads.length;
+      hb.once('sent', function () {
+        var p;
+        assert(payloads.length === (payloadCount + 1), 'payload not sent');
+        p = payloads[payloads.length - 1];
+        assert(p.error.message === 'test error 6', 'payload incorrect');
+        done();
+      });
+      hb.notify(new Error('test error 6'), { headers: sampleHeaders });
+    });
+
+    test('correctly sets the headers field in the payload', function () {
+      hb.once('sent', function () {
+        var n;
+        n = payloads[payloads.length - 1].request;
+        assert(n.cgi_data['HTTP_X_FORWARDED_FOR'] === sampleHeaders['x-forwarded-for'], 'x-forwarded-for not set');
+        assert(n.cgi_data['HTTP_USER_AGENT'] === sampleHeaders['user-agent'], 'user-agent not set');
+        assert(n.cgi_data['HTTP_COOKIE'] === sampleHeaders['cookie'], 'cookie not set');
+        assert(('headers' in n) === false, 'headers field is not removed from the payload');
+        done();
+      });
+    });
+  });
+
+  suite('Creating a Badger with both cgi_data and headers meta', function () {
+    var hb = Singleton.factory({
+      apiKey: 'fake api key'
+    });
+
+    var sampleCGIData = {
+      'server-software': 'Whatever server',
+      'custom': 'custom fields with custom DATA, !#&*'
+    };
+
+    var sampleHeaders = {
+      'user-agent': 'Mozilla 5.0',
+      'cookie': 'a=b'
+    };
+
+    test('successfully sends the payload', function (done) {
+      payloadCount = payloads.length;
+      hb.once('sent', function () {
+        var p;
+        assert(payloads.length === (payloadCount + 1), 'payload not sent');
+        p = payloads[payloads.length - 1];
+        assert(p.error.message === 'test error 6', 'payload incorrect');
+        done();
+      });
+      hb.notify(new Error('test error 6'), {
+        headers: sampleHeaders,
+        cgi_data: sampleCGIData
+      });
+    });
+
+    test('resultig payload has combined data from headers and cgi_data', function () {
+      hb.once('sent', function () {
+        var n;
+        n = payloads[payloads.length - 1].request;
+        assert(n.cgi_data['SERVER_SOFTWARE'] === sampleCGIData['server-software'], 'server-software not set');
+        assert(n.cgi_data['CUSTOM'] === sampleCGIData['custom'], 'custom  not set');
+        assert(n.cgi_data['HTTP_USER_AGENT'] === sampleHeaders['user-agent'], 'user-agent not set');
+        assert(n.cgi_data['HTTP_COOKIE'] === sampleHeaders['cookie'], 'cookie not set');
+        assert(('custom' in n) === false, 'untransformed keys from cgi_data present in payload');
+        assert(('user-agent' in n) === false, 'untransformed keys from headers present in payload');
+        assert(('headers' in n) === false, 'headers field is not removed from the payload');
+        done();
+      });
+    });
+  });
 });
