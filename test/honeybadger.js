57da543c74a2b9c6f912bfa5212533a8cec629c3
Joshua Wood <josh@hintmedia.com>
2016-04-12 11:24:13 -0700

    Report async errors from Honeybadger.wrap()

--- test/honeybadger.js
+++ test/honeybadger.js
@@ -111,6 +111,18 @@ describe('Honeybadger', function () {
       }), /Badgers!/);
       assert(Honeybadger.notify.called);
     });
+
+    it('reports async errors via #notify()', function () {
+      sinon.spy(Honeybadger, 'notify');
+      Honeybadger.wrap(function() {
+        setTimeout(function asyncThrow() {
+          throw(new Error('Badgers!'));
+        }, 0);
+      })();
+      process.nextTick(function asyncThrow() {
+        assert(Honeybadger.notify.called);
+      });
+    });
   });
 
   describe('#notify()', function () {
